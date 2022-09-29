28c11122cf9cd35641838461c90ef02e50ae3625
Rus Daiana <daiana.rus@msg.group>
2022-09-29 15:52:52 +0200

    security configs - first version

--- shop/src/main/java/ro/msg/learning/shop/services/CustomerService.java
+++ shop/src/main/java/ro/msg/learning/shop/services/CustomerService.java
@@ -2,17 +2,21 @@ package ro.msg.learning.shop.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ro.msg.learning.shop.entities.Customer;
 import ro.msg.learning.shop.repositories.CustomerRepository;
+import ro.msg.learning.shop.utils.CustomerDetails;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class CustomerService {
+public class CustomerService implements UserDetailsService {
     @Autowired
     private final CustomerRepository customerRepository;
 
@@ -33,5 +37,14 @@ public class CustomerService {
         } else {
             throw new EntityNotFoundException("Customer" + id + "not found!");
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Customer> customer = customerRepository.findByUsername(username);
+        if (customer.isPresent()) {
+            return new CustomerDetails(customer.get().getId(), customer.get().getUsername(), customer.get().getEmailAddress(), customer.get().getPassword());
+        } else
+            throw new UsernameNotFoundException("Not found: " + username);
     }
 }
